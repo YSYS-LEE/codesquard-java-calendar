dd4aed44f452538c9b26a3187618cfcc1bebd8d9
lys11 <lys11947@naver.com>
2021-07-14 17:17:22 +0900

    update

--- src/honux/calender/Sum.java
+++ src/honux/calender/Sum.java
@@ -5,13 +5,17 @@ import java.util.Scanner;
 public class Sum {
 	public static void main(String[] args) {
 		System.out.println("두 수를 입력하세요.");
+		
+		// 외부 입력값 가져오기, string으로 변환해서 숫자 하나씩 나누기
 		Scanner scanner = new Scanner(System.in);
 		String number = scanner.nextLine();
 		String[] num = number.split(" ");
 		
+		// string을 Integer로 변환
 		int first = Integer.parseInt(num[0]);
 		int second = Integer.parseInt(num[1]);
 		
+		// 출력, scanner 닫기
 		System.out.println("두 수의 합은" + (first + second)  + "입니다");
 		scanner.close();
 	}
--- src/honux/calender/Sum_video.java
+++ src/honux/calender/Sum_video.java
@@ -4,15 +4,19 @@ import java.util.Scanner;
 
 public class Sum_video {
 	public static void main(String[] args) {
+		// int 설정, 외부입력 값 받아오기
 		int a, b;
 		Scanner scanner = new Scanner(System.in);
 		String s1, s2;
 		System.out.println("두 수를 입력해주세요");
+		
+		// s1을 String으로 변환, a를 Integer로 변환
 		s1 = scanner.next();
 		s2 = scanner.next();
 		a = Integer.parseInt(s1);
 		b = Integer.parseInt(s2);
 		
+		// 출력, scanner 닫기
 		System.out.printf("%d과 %d의 값은 %d 입니다", a, b, a+b);
 		scanner.close();
 	}
